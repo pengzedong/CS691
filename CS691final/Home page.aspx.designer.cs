f0b8aae2aae9d95a6191478fd3bff8fbbd9353d2
Zedong Peng <pengzedong@gmail.com>
2018-11-14 10:13:00 -0500

    03

--- CS691final/Home page.aspx.designer.cs
+++ CS691final/Home page.aspx.designer.cs
@@ -29,5 +29,14 @@ namespace CS691final {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Label Lab1;
+        
+        /// <summary>
+        /// ListBox1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ListBox ListBox1;
     }
 }
