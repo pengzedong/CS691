50c6ebc446ad7e84e7a25a0532b496195dc5ee3a
Zedong Peng <pengzedong@gmail.com>
2018-11-16 23:17:20 -0500

    database problem

---
+++ CS691final/App_Code/menu_item_designer.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace CS691final.App_Code
+{
+    public class menu_item_designer
+    {
+        public string ID { get; set; }
+        public string FoodName { get; set; }
+        public string IngredientsInfo { get; set; }
+        public string AllergenInfo { get; set; }
+        public double CategoriesInfo { get; set; }
+        public double Price { get; set; }
+
+
+        public void ReadRecordById()
+        {
+            SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["foodCompany"].ConnectionString);
+            conn.Open();
+            string qry = "select * from Food_Menu where (id=@id)";
+            SqlCommand cmd = new SqlCommand(qry, conn);
+            cmd.Parameters.AddWithValue("@id", ID);
+            SqlDataReader dr = cmd.ExecuteReader();
+            dr.Read();
+            ID = dr["Food_id"].ToString();
+            FoodName = dr["Food_Name"].ToString();
+            IngredientsInfo = dr["Food_ingredients"].ToString();
+            AllergenInfo = dr["Food_allergen_info"].ToString();
+            CategoriesInfo = Convert.ToDouble(dr["Food_categories"].ToString());
+            Price = Convert.ToDouble(dr["Food_price"]);
+
+            dr.Close();
+            conn.Close();
+
+            
+        }
+    }
+}
---
+++ CS691final/Menu-designer.aspx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using CS691final.App_Code;
+
+
+namespace CS691final
+{
+    public partial class Menu_designer : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            menu_item_designer itemReader = new menu_item_designer();
+            itemReader.ID = "00001";
+            itemReader.ReadRecordById();
+            
+            Label1.Text = itemReader.FoodName;
+        }
+    }
+}
